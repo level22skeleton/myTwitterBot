9fa13338a69731d8075892efd1f885c94e44d2cb
level22skeleton <47456498+level22skeleton@users.noreply.github.com>
2020-09-25 13:21:04 -0300

    Updated TWEET_ACCOUNT value

--- local_settings.py
+++ local_settings.py
@@ -34,7 +34,7 @@ ODDS = 0  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
 DEBUG = False  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = "_killaaron"  # The name of the account you're tweeting to.
+TWEET_ACCOUNT = "level22bot"  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
 TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
